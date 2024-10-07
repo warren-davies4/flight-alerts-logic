c7470577ab5d177378d2021bd3a42e63b66998dd
Warren Davies <2f78fc3e-d94a-4e8f-a15c-34f89801dc65>
2024-10-07 11:35:59 +0000

    Sync remote branch at Oct 7, 2024 at 12:35PM

---
+++ transforms-python/src/myproject/datasets/raw/flight_alerts_raw.py
@@ -0,0 +1,10 @@
+# from pyspark.sql import functions as F
+from transforms.api import transform_df, Input, Output
+
+
+@transform_df(
+    Output("/SOLEX-a8870f/[Notional] RAP Playground/Data Engineering Tutorials - Code Repositories/Datasource Project: Flight Alerts/datasets/raw/flight_alerts_raw"),
+    source_df=Input("/SOLEX-a8870f/[Notional] RAP Playground/Advanced incremental data processing with PySpark in Code Repositories (2024-10-04 16:01:09)/2_multi_inputs_outputs"),
+)
+def compute(source_df):
+    return source_df
