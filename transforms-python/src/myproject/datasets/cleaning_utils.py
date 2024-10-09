c6634a226c8ae56dcf649966dfe9d9144fdc6f18
Warren Davies <2f78fc3e-d94a-4e8f-a15c-34f89801dc65>
2024-10-09 09:38:24 +0000

    added some weird formatting

--- transforms-python/src/myproject/datasets/cleaning_utils.py
+++ transforms-python/src/myproject/datasets/cleaning_utils.py
@@ -2,5 +2,10 @@ from pyspark.sql import functions as F
 
 
 def add_hello_col(df):
-    df = df.withColumn("hello", F.lit("Hello"))
+    df = (                  df
+                            .withColumn(
+                                         "hello", 
+                    F.lit("Hello")
+                )
+    )
     return df
