59e4fc5afcd872abb826afd9278d7cb55fcb379e
Warren Davies <2f78fc3e-d94a-4e8f-a15c-34f89801dc65>
2024-10-11 11:14:35 +0000

    Build Commit

--- transforms-python/src/myproject/datasets/raw/flight_alerts_raw.py
+++ transforms-python/src/myproject/datasets/raw/flight_alerts_raw.py
@@ -1,11 +1,5 @@
 # from pyspark.sql import functions as F
 from transforms.api import transform_df, Input, Output
 
-@transform_df(
-    Output("ri.foundry.main.dataset.9857288a-d355-40cb-ba4b-179d7a1cdeb1"),
-    source_df=Input(
-        "/SOLEX-a8870f/[Notional] RAP Playground/Advanced incremental data processing with PySpark in Code Repositories (2024-10-04 16:01:09)/2_multi_inputs_outputs"
-    ),
-)
 def compute(source_df):
     return source_df
