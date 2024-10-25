0db360dde4afbd06d1703616567a4d0e2d193045
Foundry (jemma) <3968f307-3a1a-4e92-8302-9ef2d767dda5>
2024-10-25 08:38:39 +0000

    Format code with Black

--- transforms-python/src/myproject/datasets/processed/flight_alerts_processed.py
+++ transforms-python/src/myproject/datasets/processed/flight_alerts_processed.py
@@ -7,7 +7,9 @@ from myproject.datasets import cleaning_utils as clean
     Output(
         "/SOLEX-a8870f/[Notional] RAP Playground/Data Engineering Tutorials - Code Repositories/Datasource Project: Flight Alerts/datasets/processed/flight_alerts_processed"
     ),
-    source_df=Input("/SOLEX-a8870f/[Notional] RAP Playground/Data Engineering Tutorials - Code Repositories/Datasource Project: Flight Alerts/datasets/preprocessed/flight_alerts_preprocessed"),
+    source_df=Input(
+        "/SOLEX-a8870f/[Notional] RAP Playground/Data Engineering Tutorials - Code Repositories/Datasource Project: Flight Alerts/datasets/preprocessed/flight_alerts_preprocessed"
+    ),
 )
 def compute(source_df):
     df_output = clean.add_processed_col(source_df)
