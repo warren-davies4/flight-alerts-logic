59fcb179c9aea5aefa43339ab76346f376722c59
Foundry (jemma) <3968f307-3a1a-4e92-8302-9ef2d767dda5>
2024-10-25 10:53:17 +0000

    Format code with Black

--- transforms-python/src/myproject/datasets/clean/flight_alerts_clean.py
+++ transforms-python/src/myproject/datasets/clean/flight_alerts_clean.py
@@ -7,11 +7,8 @@ from myproject.datasets import cleaning_utils as clean
     Output(
         "/SOLEX-a8870f/[Notional] RAP Playground/Data Engineering Tutorials - Code Repositories/Datasource Project: Flight Alerts/datasets/clean/flight-alerts-clean"
     ),
-    source_df=Input(
-        "ri.foundry.main.dataset.9857288a-d355-40cb-ba4b-179d7a1cdeb1"
-    ),
-) 
+    source_df=Input("ri.foundry.main.dataset.9857288a-d355-40cb-ba4b-179d7a1cdeb1"),
+)
 def compute(source_df):
     df_output = clean.add_clean_col(source_df)
     return df_output
- 
