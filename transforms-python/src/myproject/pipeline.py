31fc8a5d5afa3ff877eb993361252fd599fcd7b2
Warren Davies <2f78fc3e-d94a-4e8f-a15c-34f89801dc65>
2024-10-31 13:48:21 +0000

    Sync remote branch at Oct 31, 2024 at 13:48PM

--- transforms-python/src/myproject/pipeline.py
+++ transforms-python/src/myproject/pipeline.py
@@ -2,6 +2,8 @@ from transforms.api import Pipeline
 
 from myproject import datasets
 
+from myproject.datasets import clean
+
 
 my_pipeline = Pipeline()
-my_pipeline.discover_transforms(datasets)
+my_pipeline.discover_transforms(datasets.clean)
